4629c24f7b6e5b5dcabad2b3cf96eb7012f0a0a0
Gopistol <104254012+Gopistol@users.noreply.github.com>
2023-09-30 18:40:24 +0900

    Update StudyManager.java

--- StudyManager.java
+++ StudyManager.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
